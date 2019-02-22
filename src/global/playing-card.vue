fa99d79cfc7a201ddf1f932bb27ec709ae539d4d
fimion <fimion@gmail.com>
2019-02-21 21:47:15 -0500

    Updating the card looks. Starting with suit patterns.

--- src/global/playing-card.vue
+++ src/global/playing-card.vue
@@ -5,25 +5,54 @@
           :class="faceUp?'face-up':'face-down'"
           :aria-label="faceUp?name:'Face down card'">
     <base-svg>
+      <symbol id="C" viewBox="-500 -500 1000 1000" preserveAspectRatio="xMinYMid">
+        <path d="M30 150C35 385 85 400 130 500L-130 500C-85 400 -35 385 -30 150A10 10 0 0 0 -50 150A210 210 0 1 1 -124 -51A10 10 0 0 0 -110 -65A230 230 0 1 1 110 -65A10 10 0 0 0 124 -51A210 210 0 1 1 50 150A10 10 0 0 0 30 150Z" fill="black"/>
+      </symbol>
+      <symbol id="D" viewBox="-500 -500 1000 1000" preserveAspectRatio="xMinYMid">
+        <path d="M-400 0C-350 0 0 -450 0 -500C0 -450 350 0 400 0C350 0 0 450 0 500C0 450 -350 0 -400 0Z" fill="red"/>
+      </symbol>
+      <symbol id="H" viewBox="-500 -500 1000 1000" preserveAspectRatio="xMinYMid">
+        <path d="M0 -300C0 -400 100 -500 200 -500C300 -500 400 -400 400 -250C400 0 0 400 0 500C0 400 -400 0 -400 -250C-400 -400 -300 -500 -200 -500C-100 -500 0 -400 -0 -300Z" fill="red"/>
+      </symbol>
+      <symbol id="S" viewBox="-500 -500 1000 1000" preserveAspectRatio="xMinYMid">
+        <path d="M0 -500C100 -250 355 -100 355 185A150 150 0 0 1 55 185A10 10 0 0 0 35 185C35 385 85 400 130 500L-130 500C-85 400 -35 385 -35 185A10 10 0 0 0 -55 185A150 150 0 0 1 -355 185C-355 -100 -100 -250 0 -500Z" fill="black"/>
+      </symbol>
       <g v-if="faceUp">
         <text
             x="5"
-            y="1em"
-            dy="0"
+            y="15"
             class="upper corner"
             :class="color">
           {{rank}}{{symbol}}
         </text>
-        <text
-            x="10"
-            y="1em"
-            dy="50"
-            :class="color">
-          {{rank}} {{symbol}}
-        </text>
+        <g class="symbol-pattern">
+          <template v-for="(ys, x) in symbols">
+            <template v-for="(val, y) in ys">
+              <!--<text v-if="displaySymbol(val)"-->
+                  <!--:key="card.card+x+y+'symbol'"-->
+                  <!--textLength="20"-->
+                  <!--lengthAdjust="spacingAndGlyphs"-->
+                  <!--:x="Math.abs(x)"-->
+                  <!--:y="y"-->
+                  <!--:transform="upsideDown(x)"-->
+                  <!--:class="color">-->
+                <!--{{symbol}}-->
+              <!--</text>-->
+              <use v-bind:xlink:href="'#'+suit"
+                   v-if="displaySymbol(val)"
+                   :key="card.card+x+y+'symbol-use'"
+                   :x="Math.abs(x)"
+                   :y="y"
+                   :transform="upsideDown(x)"
+                   height="20"
+                   width="20"></use>
+            </template>
+          </template>
+
+        </g>
         <text
             x="5"
-            y="1em"
+            y="15"
             transform="rotate(-180 50 75)"
             class="lower corner"
             :class="color">
@@ -48,6 +77,39 @@
       },
     },
     computed: {
+      symbols(){
+        return {
+          15:{
+            20:[4,5,6,7,8,9,10],
+            50:[9,10],
+            65:[6,7,8],
+          },
+          40:{
+            20:[2,3],
+            35:[10],
+            45:[7,8],
+            65:['A',3,5,9],
+          },
+          65:{
+            20:[4,5,6,7,8,9,10],
+            50:[9,10],
+            65:[6,7,8],
+          },
+          "-15":{
+            20:[4,5,6,7,8,9,10],
+            50:[9,10],
+          },
+          '-40':{
+            20:[2,3],
+            35:[10],
+            45:[8],
+          },
+          '-65':{
+            20:[4,5,6,7,8,9,10],
+            50:[9,10],
+          },
+        }
+      },
       isEmpty(){
         return this.card.isEmpty
       },
@@ -70,6 +132,17 @@
         return this.card.readableName
       },
     },
+    methods:{
+      upsideDown(x){
+        if(x<0){
+          return "rotate(-180 50 75)"
+        }
+        return ""
+      },
+      displaySymbol(arr){
+        return arr.indexOf(this.rank)!=-1
+      },
+    },
   }
 </script>
 
@@ -86,7 +159,12 @@
     cursor:default;
   }
   .card:focus{
-    outline: var(--color-focus) 3px solid;
+    outline: none;
+    box-shadow: 0 0 5px 3px var(--color-focus);
+  }
+  .symbol-pattern{
+    font-size: 30px;
+    line-height:30px;
   }
 
   .face-up{
